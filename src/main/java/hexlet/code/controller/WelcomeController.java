204c51ee97e4bb0d35473ee0b5053a4eaf61afd9
Dangerwind <dangerwind@gmail.com>
2025-05-20 12:01:45 +0300

    прошел деплой работает как dev так и prod

--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -1,15 +1,8 @@
 package hexlet.code.controller;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import io.sentry.Sentry;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class WelcomeController {
@@ -18,8 +11,6 @@ public class WelcomeController {
     public String welcome() {
         return "Welcome to Spring!";
     }
-
-
 
     @GetMapping(path = "/test")
     public void testErrorNext() {
